96fc3524ee6c1ca47798ef317edc3c69675e4c3e
Jan Mrhar <janmrhar@Jans-MacBook-Pro-2.local>
2017-11-14 08:20:38 +0100

    removed extra query

--- app/MerchantDaily.php
+++ app/MerchantDaily.php
@@ -23,18 +23,6 @@ class MerchantDaily extends Model
                           LIMIT 1;");
     }
 
-    public static function getBestSellerByTimePeriod()
-    {
-        return DB::select("SELECT b.merchant_id AS UserID, b.analytics_date AS PeriodStart, DATE_ADD(b.analytics_date, INTERVAL 3 HOUR) AS PeriodEnd,
-                            (b.sales_total + MH1.sales_total + MH2.sales_total) AS PeriodTotalSale
-                            FROM merchant_hourly AS b 
-                              INNER JOIN merchant_hourly MH1 ON (DATE_ADD(MH1.analytics_date, INTERVAL 1 HOUR) = b.analytics_date)
-                              INNER JOIN merchant_hourly MH2 ON (DATE_ADD(MH1.analytics_date, INTERVAL 2 HOUR) = b.analytics_date)
-                              ORDER BY PeriodTotalSale DESC
-                              ");
-    }
-
-
     public static function getMerchants()
     {
         return DB::select("SELECT DISTINCT(merchant_id) FROM merchant_daily");
